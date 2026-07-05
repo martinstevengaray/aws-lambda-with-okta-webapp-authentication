07d784a3da73caef1b819054638d740d36d40456
mgaray <martinstevengaray@gmail.com>
2026-07-05 13:31:46 -0700

    add client curl script with authentication steps

---
+++ client-curl.sh
@@ -0,0 +1,20 @@
+
+#from okat admin:
+# add scope (security -> api -> add scopo) -get scope from here
+# add application (applicationa -> applications -> Create App Integration) -get clientId and client_secret from here
+
+
+OKTA_URL_PREFIX="okta_url_prefix"
+CLIENT_ID="client_id"
+CLIENT_SECRET="client-secret"
+LAMBDA_URL="lambda url"
+SCOPE="scope"
+
+TOKEN=$(curl -s "https://$OKTA_URL_PREFIX.okta.com/oauth2/default/v1/token" \
+  -u "$CLIENT_ID:$CLIENT_SECRET" \
+  -d "grant_type=client_credentials&scope=$SCOPE" | jq -r .access_token)
+
+curl -s "$LAMBDA_URL/hello?who=world" \
+      -H "Authorization: Bearer $TOKEN" \
+      -H "content-type: application/json" \
+      -d '{"greeting": "hi from curl"}' | jq
